c7e60e60be6b8f7c468fb74d9f347d73dd3cff00
Mateusz Wójtowicz <matiwojt@gmail.com>
2026-06-17 01:15:53 +0200

    rename BodyRegion to BodyRegionType, move enums to Anatomy directory

---
+++ Assets/Scripts/Anatomy/BodyRegionType.cs
@@ -0,0 +1,19 @@
+public enum BodyRegionType
+{
+    Head,
+    Torso,
+
+    LeftUpperArm,
+    RightUpperArm,
+    LeftForearm,
+    RightForearm,
+    LeftHand,
+    RightHand,
+
+    LeftThigh,
+    RightThigh,
+    LeftLowerLeg,
+    RightLowerLeg,
+    LeftFoot,
+    RightFoot
+}
---
+++ Assets/Scripts/Anatomy/HandJointType.cs
@@ -0,0 +1,29 @@
+public enum HandJointType
+{
+    Wrist = 0,
+
+    ThumbCMC = 1,
+    ThumbMP = 2,
+    ThumbIP = 3,
+    ThumbTip = 4,
+
+    IndexMCP = 5,
+    IndexPIP = 6,
+    IndexDIP = 7,
+    IndexTip = 8,
+
+    MiddleMCP = 9,
+    MiddlePIP = 10,
+    MiddleDIP = 11,
+    MiddleTip = 12,
+
+    RingMCP = 13,
+    RingPIP = 14,
+    RingDIP = 15,
+    RingTip = 16,
+
+    LittleMCP = 17,
+    LittlePIP = 18,
+    LittleDIP = 19,
+    LittleTip = 20
+}
